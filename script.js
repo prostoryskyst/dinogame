3becabd4761b4352fabdbb484f30341beff1a055
prostoryskyst <ruslan.miropolets@gmail.com>
2023-08-16 21:52:29 +0300

    first

---
+++ script.js
@@ -0,0 +1,27 @@
+const dino = document.getElementById("dino")
+const cactus = document.getElementById("cactus")
+
+
+  document.addEventListener("keydown",function(event){
+    jump();
+});
+
+  function jump() {
+    if (dino.classList !="jump") {
+      dino.classList.add("jump")
+}
+
+    setTimeout (function () {
+    dino.classList.remove("jump")
+  },3000)
+    
+}
+  let isAlive = setInterval(function (params) {
+    let dinotop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+    let cactusleft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+
+    if(cactusleft<50 && cactusleft>0 && dinotop>=140){
+        alert("GAME OVER!!!")
+    }
+
+})
